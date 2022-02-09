2821f12492209117e73cc4278cce8c06e8251e9e
Maksim Grishkin <fffff@bk.ru>
2022-02-09 12:19:52 +0300

    test(parser): Добавляет тесты

---
+++ mapinfow_prj_tests/doctest_run.py
@@ -0,0 +1,9 @@
+import doctest
+
+import mapinfow_prj.parser as parser
+
+
+def load_tests(loader, tests, ignore):
+    tests.addTests(doctest.DocFileSuite("parser.txt"))
+    tests.addTests(doctest.DocTestSuite(parser))
+    return tests
